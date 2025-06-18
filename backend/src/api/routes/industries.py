662c86692cd6bfa6321ff8616a316105e2450771
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-06-18 10:26:00 +0100

    industry mock added

--- backend/src/api/routes/industries.py
+++ backend/src/api/routes/industries.py
@@ -1,11 +1,23 @@
 # from typing import List
 from fastapi import APIRouter
+from sqlmodel import select
 
 # from sqlmodel import select
 from src.models.home_models import (
+    HomeService,
     create_table,
+)
+from utils import (
+    get_session,
 )  # , HomeService, HomeTestimonial, ServiceTechStack, HomeIndustry, HomeProcess, FAQ
 # from src.utils import get_session
 
 route = APIRouter(prefix="/industries")
 route.add_event_handler("startup", create_table)
+
+
+@route.get("/get-industry-services")
+def get_industry_service(industry: str):
+    with get_session() as session:
+        stmt = select(HomeService).where(HomeService.description == industry)
+        session.exec(stmt)
