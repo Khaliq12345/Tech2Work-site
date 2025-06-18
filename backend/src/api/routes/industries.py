68388d7b298680552e06e78c24e1ee24f9778b90
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-06-18 10:20:33 +0100

    frontend and backend edited

--- backend/src/api/routes/industries.py
+++ backend/src/api/routes/industries.py
@@ -1,8 +1,11 @@
-#from typing import List
+# from typing import List
 from fastapi import APIRouter
-#from sqlmodel import select
-from src.models.home_models import create_table #, HomeService, HomeTestimonial, ServiceTechStack, HomeIndustry, HomeProcess, FAQ
-#from src.utils import get_session
+
+# from sqlmodel import select
+from src.models.home_models import (
+    create_table,
+)  # , HomeService, HomeTestimonial, ServiceTechStack, HomeIndustry, HomeProcess, FAQ
+# from src.utils import get_session
 
 route = APIRouter(prefix="/industries")
 route.add_event_handler("startup", create_table)
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxt/icon'],
-  css: ['~/assets/css/main.css'],
-})
+  modules: ["@nuxt/ui", "@nuxt/icon"],
+  css: ["~/assets/css/main.css"],
+  runtimeConfig: {
+    API_URL: process.env.API_URL,
+  },
+});
+
