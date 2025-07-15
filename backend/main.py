fede2278a8f1644174379765828672d13768278a
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-07-15 17:16:17 +0100

    env option added

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,12 @@
 import uvicorn
+from src.core.config import ENV
 
 
 if __name__ == "__main__":
-    uvicorn.run("src.api.app:app", reload=True, port=8000, host="0.0.0.0")
+    uvicorn.run(
+        "src.api.app:app",
+        reload=ENV == "dev",
+        port=8000,
+        host="0.0.0.0",
+        workers=5,
+    )
