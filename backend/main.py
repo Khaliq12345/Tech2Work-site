c79345869a74e377ba811c0fcd1c9437f943c948
Hafiz <abdulmoustapha64@@gmail.com>
2025-09-18 12:03:26 +0100

    Internationalization i18n completed

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "src.api.app:app",
         reload=ENV == "dev",
-        port=8000,
-        host="0.0.0.0",
+        port=8001,
+        host="localhost",
         workers=5,
     )
